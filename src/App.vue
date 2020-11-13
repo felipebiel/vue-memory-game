b6a114bf811ac9c3253f7ac57323a693dcb8d915
Felipe Biel <felipe.biel.souza@gmail.com>
2020-11-13 11:22:50 -0300

    refactor: Limpando a home base

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Novo Jogo</router-link> |
+      <router-link to="/about">Lista de melhores</router-link>
     </div>
     <router-view/>
   </div>
