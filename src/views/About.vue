b56eee86bbd5a32afc027d719b70478072a57c79
Felipe Biel <felipe.biel.souza@gmail.com>
2020-11-14 21:48:55 -0300

    new: Rank com os usuários salvos no localStorange

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Melhores Jogadores</h1>
+    <ul>
+      <li v-for="(user, index) in rankUsers" :key="index">
+        {{ user.name }} - {{ user.moves}}
+      </li>
+    </ul>
   </div>
 </template>
+<script>
+  export default {
+    
+   computed: {
+     rankUsers() {
+       return JSON.parse(localStorage.getItem('usersRank')) ? JSON.parse(localStorage.getItem('usersRank')).sort((a, b) => a.moves - b.moves) : [];
+     }
+   },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
