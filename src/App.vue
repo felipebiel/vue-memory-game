84128d2692595a655d5b89154473b7447690bd5c
Felipe Biel <felipe.biel.souza@gmail.com>
2020-11-15 15:13:31 -0300

    refactor: Diminuir o time da animação

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ body {
 }
 
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+  transition: opacity .2s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
